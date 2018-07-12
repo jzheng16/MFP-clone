5ca698a258364b8500250447df482bf9b43e70eb
Joey <jxzheng16@gmail.com>
2018-07-12 00:33:13 -0400

    style: tested some styled components on homepage

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -2,47 +2,99 @@ import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+
 const Title = styled.h1`
+  position: relative;
+  top: -400px;
   font-size: 1.5em;
   text-align: center;
-  color: red;
+  color: orange;
 `;
-const Title1 = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: blue;
+const MiscInfo = styled.p`
+  text-align: left;
+  font-family: serif;
+  text-transform: none;
+`;
+
+const UserDisplayInfo = styled.ul`
+  list-style: none;    /* list-style shorthand for list-style-type image position */
+  padding-left: 2rem;
+`;
+
+const InfoList = styled.li`
+`;
+
+const HomeHeader = styled.div`
+  position: relative;
+`;
+
+const HomeImage = styled.img`
+  display: block;
+  max-width: 40%;
+  height: auto;
+  margin: 0 auto;
+  z-index: -1;
+`;
+
+const StyledLink = styled(Link)`
+
+  &:link{
+    color: #265301;
+  }
+  &:hover {
+    border-bottom: 1px transparent;     
+    background: #CDFEAA;
+  }
+  
+`;
+
+const StyledButton = styled.button`
+  color: black;
+  font-size: 1em;
+  margin: 1em;
+  padding: .25em 1em;
+  border-radius: 2px;
 `;
 
 const Home = props => (
-  <div className="home">
-    {
-      props.goal && props.user.weight ?
-        <div>
-          <Title> Welcome to MyFitnessClone!</Title> <br></br>
-          <i> This page will show your calorie goal and how much you&#39;ve accomplished </i> <br></br>
-          <i> Your current weight and your macro split</i> <br></br>
-          <b> Weight: {props.user.weight[props.user.weight.length - 1]} </b> <br></br>
-          <b> Calories: {props.goal.calorie} </b> <br></br>
-          <b> Carbs: {props.goal.carbs} </b> <br></br>
-          <b> Protein: {props.goal.protein} </b> <br></br>
-          <b> Fat: {props.goal.fat} </b> <br></br>
-          <i> Will have a link to get started if you are new </i> <br></br>
-          <br></br>
-          <Link to="/addfood"> Add Food </Link>
-          <br></br>
-          <Link to="/goal"> Click here to get started! </Link>
-        </div>
-        :
-        <div>
-          <Title1> Welcome to MyFitnessClone! </Title1> <br></br>
-          <i> This page will show your calorie goal and how much you&#39;ve accomplished </i> <br></br>
-          <i> Your current weight and your macro split</i> <br></br>
-          <i> Will have a link to get started if you are new </i> <br></br>
-          <Link to="/login"> Log-in to get started! </Link> <br></br>
-        </div>
+  <HomeHeader>
+    <HomeImage src={require("../../public/homeImage.png")} />
+    <Title> Welcome to MyFitnessClone!</Title>
+    <hr />
+    {props.goal && props.user.weight ?
+      <div>
+        <h2> Welcome back {props.user.first_name} {props.user.last_name}! </h2>
+        <MiscInfo>
+          This page will show your calorie goal and how much you&#39;ve accomplished <br></br>
+          Your current weight and your macro split <br></br>
+        </MiscInfo>
+
+        <UserDisplayInfo>
+          <InfoList> Weight: {props.user.weight[props.user.weight.length - 1]} </InfoList>
+          <InfoList> Calories: {props.goal.calorie} </InfoList>
+          <InfoList> Carbs: {props.goal.carbs} </InfoList>
+          <InfoList> Protein: {props.goal.protein} </InfoList>
+          <InfoList> Fat: {props.goal.fat} </InfoList>
+        </UserDisplayInfo>
+
+        <StyledLink to="/addfood"> <StyledButton > Add Food </StyledButton> </StyledLink>
+
+        <StyledLink to="/goal"> Edit Goals </StyledLink>
+
+      </div>
+      :
+      <div>
+        <MiscInfo>
+          This page will show your calorie goal and how much you&#39;ve accomplished <br></br>
+          Your current weight and your macro split <br></br>
+          Will have a link to get started if you are new  <br></br>
+        </MiscInfo>
+        <Link to="/login"> Log-in to get started! </Link> <br></br>
+      </div>
+
     }
-  </div>
+  </HomeHeader>
 
 );
 
-module.exports = Home;
+export default Home;
